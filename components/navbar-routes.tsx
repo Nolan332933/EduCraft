dbad0eec8a032335aabcc4a710ae6d1987f18ef7
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-06-29 01:48:02 +0700

    Dynamic Layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,36 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+  const pathName = usePathname();
+
+  const isTeacherPage = pathName?.startsWith("/teacher");
+  const isPlayerPage = pathName?.includes("/chapter");
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button
+            size="sm"
+            className="bg-blue-400 text-white hover:bg-blue-500"
+          >
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
