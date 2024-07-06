f2219770eb641437bd3fbf7b3ebda1761a9669d2
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-07 05:28:00 +0700

    Update course title section

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -47,6 +48,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <IconBadge size="sm" icon={LayoutDashboard} />
             <h2 className="text-2xl">Customize your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
