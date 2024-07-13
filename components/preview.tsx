ae9629274fe7f763cb90901f3d780a37114b0e22
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-14 01:20:08 +0700

    Chapter Description Form

---
+++ components/preview.tsx
@@ -0,0 +1,19 @@
+"use client";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import "react-quill/dist/quill.bubble.css";
+
+interface PreviewProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+export const Preview = ({ onChange, value }: PreviewProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+
+  return <ReactQuill theme="bubble" value={value} readOnly />;
+};
