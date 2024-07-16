221c064ef5593fb898beef64174f75d8036a74e5
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-16 13:46:53 +0700

    Chapter unpublish

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -0,0 +1,66 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      courseId: string;
+      chapterId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+    if (!ownCourse) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const unpublishedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    const publishedChapterInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChapterInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(unpublishedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_UNPUBLISHED]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
