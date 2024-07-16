afed463b695f9cad7fbc21e0f2402834c9ff00d0
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-16 13:14:27 +0700

    publish course

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,6 +8,85 @@ const mux = new Mux({
   tokenSecret: process.env.MUX_TOKEN_SECRET,
 });
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: chapter.id,
+        },
+      });
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
+
+    const publisheChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publisheChaptersInCourse) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
---
+++ components/banner.tsx
@@ -0,0 +1,38 @@
+import { AlertTriangle, CheckCircleIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const bannerVariants = cva(
+  "border text-center p-4 text-sm flex items-center w-full",
+  {
+    variants: {
+      variant: {
+        warning: "bg-yellow-200/80 border-yellow-30 text-primary",
+        success: "bg-emerald-700 border-emeral-800 text-secondary",
+      },
+    },
+    defaultVariants: {
+      variant: "warning",
+    },
+  }
+);
+
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+  label: string;
+}
+
+const iconMap = {
+  warning: AlertTriangle,
+  success: CheckCircleIcon,
+};
+
+export const Banner = ({ label, variant }: BannerProps) => {
+  const Icon = iconMap[variant || "warning"];
+
+  return (
+    <div className={cn(bannerVariants({ variant }))}>
+      <Icon className="w-6 h-6 mr-2" />
+      <span>{label}</span>
+    </div>
+  );
+};
