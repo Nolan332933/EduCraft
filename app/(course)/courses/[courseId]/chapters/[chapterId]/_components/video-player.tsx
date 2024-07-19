28c184e6ef124d9b474f70696245e23a04b76bf8
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-07-20 04:21:22 +0700

    Chapter HLS video player

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { useState } from "react";
+import { Toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Loader2, Lock } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+
+interface VideoPlayerProps {
+  playbackId: string;
+  courseId: string;
+  chapterId: string;
+  nextChapterId?: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+  title: string;
+}
+
+export const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completeOnEnd,
+  title,
+}: VideoPlayerProps) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <div className="relative aspect-ratio">
+      {!isPlaying && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8 " />
+          <p className="text-center text-sm">This video is locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isPlaying && "hidden")}
+          onCanPlay={() => setIsPlaying(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
+};
